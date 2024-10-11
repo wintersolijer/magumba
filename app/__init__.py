a86eff6f6ffaad17ca66434c0800de648445ea64
aristoteles78 <aris.hofmann@gmail.com>
2024-10-11 12:32:27 +0200

    Add frontend login  with React and Tailwind

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from .utils import DbHandler, PeopleHandler, QuestionHandler
 
-
 app = Flask(__name__)
+CORS(app)  # CORS aktivieren
 
 PEOPLE_HANDLER = PeopleHandler()
 QUESTION_HANDLER = QuestionHandler()
 
 from app import routes
+
