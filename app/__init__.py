4cebdb200e7e296170362c8e21d32002d1ba4d14
Adam <wi23315@lehre.dhbw-stuttgart.de>
2024-11-06 11:31:23 +0100

    skibidi

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from .utils import DbHandler, PeopleHandler, QuestionHandler, CourseHandler
+from .utils import PeopleHandler, QuestionHandler, CourseHandler
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 PEOPLE_HANDLER = PeopleHandler()
 QUESTION_HANDLER = QuestionHandler()
